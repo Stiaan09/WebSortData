7ff2fc17da9ac84a29bbadf7c4fe6a9f3dd26cd1
Stiaan <stiaankruger9@gmail.com>
2020-11-17 16:29:53 +0200

    Changed firebase files location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+//import { firebase } from '@angular/core'; // gaan install library op firebase website npm i
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WebSortData';
+    firebaseConfig = {
+    apiKey: "AIzaSyB2XENUEVZIdanjtKklmoWUCVbNPawoQF0",
+    authDomain: "krugertroue-f1492.firebaseapp.com",
+    databaseURL: "https://krugertroue-f1492.firebaseio.com",
+    projectId: "krugertroue-f1492",
+    storageBucket: "krugertroue-f1492.appspot.com",
+    messagingSenderId: "986932919788",
+    appId: "1:986932919788:web:6466dcb31e7119be91d4c9"
+}; 
+
+// Initialize Firebase
+//   constructor(firebase.initializeApp)(firebaseConfig); // moet in n constructor wees 
+
+//    writeData(){
+//     // firebase.database().ref("User").set({
+//     //     name: document.getElementById("nameField").value,
+//     //     attend: document.getElementById("attendField").value
+//     // });
+//     console.log("String Hello");
+// }
+
 }
